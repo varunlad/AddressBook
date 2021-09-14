420103a5066ad635a736c460e86764f1b7e8d955
varun <varunlad5@gmail.com>
2021-09-14 20:07:30 +0530

    [varun]New Contact can be Added

--- AddressBook/Address.cs
+++ AddressBook/Address.cs
@@ -8,5 +8,23 @@ namespace AddressBook
 {
     class Address
     {
+        public string firstname;
+        public string lastname;
+        public string address;
+        public string city;
+        public string state;
+        public string zipcode;
+        public string email;
+
+        public Address(string firstname, string lastname, string address, string city, string state, string zipcode, string email)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipcode = zipcode;
+            this.email = email;
+        }
     }
 }
